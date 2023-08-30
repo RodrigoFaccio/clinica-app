4d01e5dc8c66c0ed05308408f4bc7ad5aaca373a
RodrigoFaccio <rodrigo.facio@yogha.com>
2023-08-30 15:31:29 -0300

    new screens

--- src/pages/SearchPatients/styles.ts
+++ src/pages/SearchPatients/styles.ts
@@ -1,9 +1,17 @@
 import styled from 'styled-components/native';
 
+<<<<<<< HEAD
 export const Container = styled.View`
   justify-content: center;
   align-items: center;
   flex: 1;
+=======
+export const Container = styled.KeyboardAvoidingView`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  gap: 20px;
+>>>>>>> cb12bbd (new screens)
 `;
 export const Image = styled.Image`
   width: 90%;
